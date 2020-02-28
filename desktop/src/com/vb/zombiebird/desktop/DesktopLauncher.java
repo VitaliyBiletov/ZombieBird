d4d73aef26b60d0ae16bbaac47fdc9bfa1e19dee
Vitaliy Biletov <vit.biletov@gmail.com>
2020-02-28 13:00:26 +0300

    add GameScreen and ZBGame classes

--- desktop/src/com/vb/zombiebird/desktop/DesktopLauncher.java
+++ desktop/src/com/vb/zombiebird/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.vb.zombiebird.ZBGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "ZombieBird";
+		config.width = 480;
+		config.height = 320;
 		new LwjglApplication(new ZBGame(), config);
+
 	}
 }
