30262a9fb05a0d2e4aacc6a06665844b5df9d6af
Vitaliy Biletov <vit.biletov@gmail.com>
2020-02-28 12:31:04 +0300

    add all

---
+++ desktop/src/com/vb/zombiebird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.vb.zombiebird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.vb.zombiebird.ZBGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZBGame(), config);
+	}
+}
