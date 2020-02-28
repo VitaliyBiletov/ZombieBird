9b71c7c2a50fdd51c7f945c8c113f1358d45003a
Vitaliy Biletov <vit.biletov@gmail.com>
2020-02-28 17:31:40 +0300

    Создание класса Scrollable и его двух дочерних классов Grass и Pipe

--- core/src/com/vb/gameobjects/Scrollable.java
+++ core/src/com/vb/gameobjects/Scrollable.java
@@ -1,4 +1,54 @@
 package com.vb.gameobjects;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class Scrollable {
+    protected Vector2 position;
+    protected Vector2 velocity;
+    protected int width;
+    protected int height;
+    protected boolean isScrollLeft;
+
+    public Scrollable(float x, float y, int width, int height, float scrollSpeed){
+        position = new Vector2(x, y);
+        velocity = new Vector2(scrollSpeed, 0);
+        this.width = width;
+        this.height = height;
+        isScrollLeft = false;
+    }
+
+    public void update(){
+        if(position.x + width < 0){
+            isScrollLeft = true;
+        }
+    }
+
+    public void reset(float newX){
+        position.x = newX;
+        isScrollLeft = false;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public Vector2 getVelocity() {
+        return velocity;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public boolean isScrollLeft() {
+        return isScrollLeft;
+    }
+
+    public float getTailX(){
+        return position.x + width;
+    }
 }
